aed59463863b13a53761e6528e0daabb95710ffd
Roman Ivanov <romanivanov21@mail.ru>
2016-02-25 01:12:54 +0300

    Исправлен предыдущий коммит. Добавлен интерфейс.

---
+++ application/Interface/IViewNavButtons.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace application.Interface
+{
+    internal interface IViewNavButtons
+    {
+        event EventHandler FourClick;
+        event EventHandler NextClick;
+        event EventHandler OneClick;
+        event EventHandler PrevClick;
+        event EventHandler ThreClick;
+        event EventHandler TwoClick;
+    }
+}
